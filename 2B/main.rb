6ea1b0a7b8189ed534549f176a837978f0698c5b
Gustavo Kath <gustavo.kath@sap.com>
2018-12-02 20:13:58 -0200

    Solved 2B

--- 2B/main.rb
+++ 2B/main.rb
@@ -1,30 +1,26 @@
-sum = 0
-count_two = 0
-count_three = 0
-lines = $stdin.each_line.map(&:to_s)
+def main
+  string = ""
+  lines = $stdin.each_line.map(&:chomp)
 
-for i in 0..lines.length-2
-  for j in i+1..lines.length-1
-    dif = 0
+  for i in 0..lines.length-2
     first_word = lines[i]
-    next_word = lines[j]
+    for j in i+1..lines.length-1
+      dif = 0
+      next_word = lines[j]
 
-    #puts first_word
-    #puts next_word
-    for k in 0..first_word.length
-      dif += 1 if first_word[k] != next_word[k]
-    end
+      for k in 0..first_word.length-1
+        dif += 1 if first_word[k] != next_word[k]
+      end
 
-    #puts dif
-
-    if dif < 1
-      puts "AQUI"
-      for k in 0..first_word.length
-        if first_word[k] == next_word[k]
-          print first_word[k]
+      if dif == 1
+        for k in 0..first_word.length-1
+          string += first_word[k] if first_word[k] == next_word[k]
         end
+
+        return string
       end
     end
   end
-
 end
+
+puts main
