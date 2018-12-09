8b71016df50c6e125eb3db51e3a3b40f49e47349
Gustavo Kath <gustavo.kath@acad.pucrs.br>
2018-12-09 16:39:38 -0200

    Solved 9A

--- 9A/main.rb
+++ 9A/main.rb
@@ -7,42 +7,21 @@ players = Array.new(n_players){0}
 
 next_marble = 1
 current_player = 0
-current_marble = 0
 current_marble_index = 0
 max_score = 0
 
-p n_players
-p last
-p marbles
-p players
-
 while next_marble <= last
-    p next_marble
-    p current_marble_index 
-    p marbles.length
-    p '---------'
-    next_position = (current_marble_index + 1) % marbles.length
-    next_position += 1 if next_position == 0
+    next_position = (current_marble_index + 2) % (marbles.length + 1)
+    next_position = 1 if next_position == 0
     if next_marble % 23 == 0
-        p "Adding for player #{current_player}"
-        players[current_player] += next_marble
-        p players[current_player]
-        p next_marble
-        p next_position
-        p marbles
-        remove_index = (next_position - 7) % marbles.length
+        players[current_player] += next_marble 
+        remove_index = (current_marble_index - 7) % marbles.length
         removed = marbles.delete_at(remove_index)
-        p removed
-        p '-----------'
         players[current_player] += removed
         current_marble_index = remove_index
-        current_marble = marbles[remove_index]
-        if players[current_player] > max_score
-            max_score = players[current_player] 
-        end
+        max_score = players[current_player]  if players[current_player] > max_score
     else
         marbles.insert(next_position, next_marble)
-        current_marble = next_marble
         current_marble_index = next_position
     end
     current_player = (current_player + 1) % players.length
