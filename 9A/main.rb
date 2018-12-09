dda3f74815441254facce7e7b9dd2ef11518fcfb
Gustavo Kath <gustavo.kath@acad.pucrs.br>
2018-12-09 17:12:23 -0200

    Improved 9A time complexity

--- 9A/main.rb
+++ 9A/main.rb
@@ -1,28 +1,51 @@
+class Node
+    attr_accessor :value, :prev, :next
+
+    def initialize(value, prev=nil, nxt=nil)
+        @value = value
+        @prev = prev
+        @next = nxt
+    end
+end
+
+def remove_seven_before(node, count=0)
+    return remove_seven_before(node.prev, count+1) if count != 7
+
+    node.prev.next = node.next
+    node.next.prev = node.prev
+    node
+end
+
 input = $stdin.readline.chomp.split(' ')
 n_players = input[0].to_i
 last = input[6].to_i
 
-marbles = [0]
+current_marble = Node.new(0)
+current_marble.prev = current_marble
+current_marble.next = current_marble
 players = Array.new(n_players){0}
 
 next_marble = 1
 current_player = 0
-current_marble_index = 0
 max_score = 0
 
 while next_marble <= last
-    next_position = (current_marble_index + 2) % (marbles.length + 1)
-    next_position = 1 if next_position == 0
     if next_marble % 23 == 0
-        players[current_player] += next_marble 
-        remove_index = (current_marble_index - 7) % marbles.length
-        removed = marbles.delete_at(remove_index)
-        players[current_player] += removed
-        current_marble_index = remove_index
-        max_score = players[current_player]  if players[current_player] > max_score
+        players[current_player] += next_marble
+        removed = remove_seven_before(current_marble)
+        players[current_player] += removed.value
+        current_marble = removed.next
+        if players[current_player] > max_score
+            max_score = players[current_player] 
+        end
     else
-        marbles.insert(next_position, next_marble)
-        current_marble_index = next_position
+        new_marble = Node.new(next_marble)
+        new_marble.prev = current_marble.next
+        new_marble.next = current_marble.next.next
+
+        new_marble.prev.next = new_marble
+        new_marble.next.prev = new_marble
+        current_marble = new_marble
     end
     current_player = (current_player + 1) % players.length
     next_marble += 1
